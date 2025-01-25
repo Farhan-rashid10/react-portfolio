bdbf7c0f329ac5c79ae16b88b3438ece62b9e25d
Farha-rashid-10 <farkaf07@gmail.com>
2025-01-25 12:05:32 +0300

    finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         </div>
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Web Developer
+            I'm a Full Stack software Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have experience of building websites and desgining software.
